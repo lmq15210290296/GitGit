7f93e6b194ecde08c7ac77a39d4ef516a8c2978f
lmq15210290296 <1350081136@qq.com>
2016-04-30 01:47:36 +0800

    udate file 02

--- src/com/atguigu/git/HelloGit.java
+++ src/com/atguigu/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit01");
 		System.out.println("HelloGit02");
+		System.out.println("HelloGit03");
 	}
 	
 }
